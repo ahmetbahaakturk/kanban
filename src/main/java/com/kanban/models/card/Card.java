44aa5210d6390ec6705b68851b1ebb04b95c1a32
Ahmet Baha Aktürk <ahmetbahaakturk1@outlook.com>
2026-07-14 10:01:28 +0300

    schema: restore card position constraint

--- src/main/java/com/kanban/models/card/Card.java
+++ src/main/java/com/kanban/models/card/Card.java
@@ -11,11 +11,18 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Table(name = "cards")
+@Table(
+    name = "cards",
+    uniqueConstraints = @UniqueConstraint(
+        name = "uq_cards_task_list_position",
+        columnNames = {"task_list_id", "position"}
+    )
+)
 @Getter
 @Setter
 public class Card {
--- src/main/resources/db/migration/V2__remove_cards_task_list_position_constraint.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE cards
-    DROP CONSTRAINT IF EXISTS uq_cards_task_list_position;
